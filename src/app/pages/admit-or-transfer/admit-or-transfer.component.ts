7bd5e558a7546b3b0e35919b4896e782933e8b43
Nisuga Jayawardana <44756803+NisugaJ@users.noreply.github.com>
2023-08-03 14:49:27 +0100

    Commit from TeleportHQ

--- src/app/pages/admit-or-transfer/admit-or-transfer.component.ts
+++ src/app/pages/admit-or-transfer/admit-or-transfer.component.ts
@@ -7,7 +7,7 @@ import { Title, Meta } from '@angular/platform-browser'
   styleUrls: ['admit-or-transfer.component.css'],
 })
 export class AdmitOrTransfer {
-  rawjmos: string = ' '
+  raws1p9: string = ' '
   constructor(private title: Title, private meta: Meta) {
     this.title.setTitle('Admit-or-Transfer - gsdp-g4-hms')
     this.meta.addTags([
